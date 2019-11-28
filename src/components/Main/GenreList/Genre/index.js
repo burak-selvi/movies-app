998318b5a567813202715d83c924b2a60ec43228
Burak Selvi <burakselvi858@hotmail.com>
2019-11-28 13:42:34 +0300

    folder structure made

---
+++ src/components/Main/GenreList/Genre/index.js
@@ -0,0 +1,2 @@
+import Genre from './Genre';
+export { Genre };
---
+++ src/components/Main/GenreList/GenreList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Genre from './Genre';
+
+export default function GenreList() {
+  return (
+    <div>
+      <Genre />
+    </div>
+  )
+}
