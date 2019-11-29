9b4ba692c6705535bb6819d7c9a3fce6c5de3695
Burak Selvi <burakselvi858@hotmail.com>
2019-11-29 19:50:12 +0300

    routes added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Route, Switch, Redirect } from "react-router-dom";
+import { Movies, Customers, Rentals, Login, NotFound } from '../../pages';
 
 export default function Main() {
   return (
-    <main className="container">
-
-    </main>
+    <Switch>
+      <Route path="/movies" component={Movies} />
+      <Route path="/customers" component={Customers} />
+      <Route path="/rentals" component={Rentals} />
+      <Route path="/login" component={Login} />
+      <Route path="/not-found" component={NotFound} />
+      <Redirect exact from="/" to="/movies" />
+      <Redirect to="/not-found" />
+    </Switch>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Movies from './Movies';
+import Customers from './Customers';
+import Rentals from './Rentals';
+import Login from './Login';
+import NotFound from './NotFound';
+
+export { Movies, Customers, Rentals, Login, NotFound };
